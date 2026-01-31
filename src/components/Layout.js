c8b62327a7200653b0fb2568901419107f4a7ca6
Aviral Shukla <aviral.shukla2122000@gmail.com>
2026-02-01 00:25:33 +0530

    Add responsive padding to Layout component and ensure body overflow handling

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Layout = ({ children, className = "" }) => {
   return (
-    <div className={`w-full h-full inline-block z-0 bg-light dark:bg-dark p-10 ${className}`}>
+    <div className={`w-full h-full inline-block z-0 bg-light dark:bg-dark sm:px-8 p-10 ${className}`}>
       {children}
     </div>
   );
